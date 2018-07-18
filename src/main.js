a6fe78c12da237d66e02656d4e3affee4b47ac55
Matt Duffield <matt.duffield@gmail.com>
2018-07-17 18:29:41 -0700

    Auto commit from FrontEnd Creator

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import 'babel-polyfill';
 import * as Bluebird from 'bluebird';
-// import 'jquery';
-// import 'bootstrap';
+import 'jquery';
+import 'bootstrap';
 
 export async function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging();
-    // .plugin('aurelia-bootstrap-select');
+    .developmentLogging()
+    .plugin('aurelia-bootstrap-select');
 
   await aurelia.start();
   await aurelia.setRoot('app');
