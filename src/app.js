a39a4e4e618c699e45a7b89224390672ea7a4a54
Matt Duffield <matt.duffield@gmail.com>
2018-07-17 18:23:10 -0700

    Auto commit from FrontEnd Creator

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia Babel SystemJS';
     config.map([
-      {"route": ["", "welcome"], "name": "welcome", "moduleId": "./welcome", "nav": true, "title": "Welcome"},
+      {"route": ["", "welcome"], "name": "welcome", "moduleId": "./views/welcome", "nav": true, "title": "Welcome"},
       {"route": "bootstrap-select", "name": "bootstrap-select", "moduleId": "./views/bootstrap-select/bootstrap-select", "nav": true, "title": "Bootstrap Select"}
     ]);
 
