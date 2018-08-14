89bda17782a1e92a17e8d2baf0796c240e4aa0a1
Matt Duffield <matt.duffield@gmail.com>
2018-08-14 12:20:44 -0700

    Auto commit from FrontEnd Creator

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ export async function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging()
-    // .plugin('aurelia-bootstrap-select');
-    .plugin('aurelia-bootstrap-tagsinput');
+    .plugin('aurelia-bootstrap-select');
+    // .plugin('aurelia-bootstrap-tagsinput');
 
   await aurelia.start();
   await aurelia.setRoot('app');
