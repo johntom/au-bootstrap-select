b807e3edf003879c2cfac6c8da32fbeeda00b007
Matt Duffield <matt.duffield@gmail.com>
2018-09-06 08:10:28 -0700

    Auto commit from FrontEnd Creator

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ export class App {
     config.title = 'Aurelia Babel SystemJS';
     config.map([
       {"route": ["", "welcome"], "name": "welcome", "moduleId": "./views/welcome/welcome", "nav": true, "title": "Welcome"},
-      {"route": "bootstrap-select", "name": "bootstrap-select", "moduleId": "./views/bootstrap-select/bootstrap-select", "nav": true, "title": "Bootstrap Select"}
+      {"route": "bootstrap-select", "name": "bootstrap-select", "moduleId": "./views/bootstrap-select/bootstrap-select", "nav": true, "title": "Bootstrap Select"},
+      {"route": "aub-typeahead", "name": "aub-typeahead", "moduleId": "./views/aub-typeahead/aub-typeahead", "nav": true, "title": "Bootstrap Typeahead"}
     ]);
 
     this.router = router;
---
+++ src/views/aub-typeahead/aub-typeahead.js
@@ -0,0 +1,11 @@
+import 'bootstrap-select/css/bootstrap-select.min.css';
+
+export class AubTypeahead {
+
+  daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+
+  constructor() {
+
+  }
+}
